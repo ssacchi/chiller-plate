aec247e26d9922e9dc10aaf3ae247f3fc7ceca1c
Morgan Dean <morgandeanhi@gmail.com>
2019-07-15 11:37:47 -1000

    first page

--- js/init.js
+++ js/init.js
@@ -1,8 +1,16 @@
+const G_TRACKING_ID = 'google tracking id';
+
 (function($){
   $(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', G_TRACKING_ID);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
